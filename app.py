6f587370059ff6de003b40e520ac9ccfb16bf610
erendenizozer <denizerenozer@icloud.com>
2025-11-06 15:34:10 +0000

    Initial habit tracker

---
+++ app.py
@@ -0,0 +1,39 @@
+habits = []
+
+def add_habit(name):
+    habits.append({"name": name, "days": 0})
+
+def complete_habit(name):
+    for habit in habits:
+        if habit["name"] == name:
+            habit["days"] += 1
+
+def show_habits():
+    for habit in habits:
+        print(f"{habit['name']} — {habit['days']} days")
+
+def menu():
+    while True:
+        print("\n1. Add Habit\n2. Complete Habit\n3. Show Progress\n4. Quit")
+        try:
+            input_num = int(input("Enter a number: "))
+            if input_num == 1:
+                new_habit = input("Enter your new habit: ")
+                add_habit(new_habit)
+                print(f"Added new habit: {new_habit}")
+            elif input_num == 2:
+             completed_habit = input("Enter the complated habits name:")
+             complete_habit(completed_habit)
+            elif input_num == 3:
+             show_habits()
+            elif input_num == 4:
+             print("Bye")
+             break
+            else:
+             print("Choose a number 1-4")
+        except ValueError:
+            print("Please enter a valid number.")
+
+             
+menu()
+
